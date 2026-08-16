180ca32f2885cb96729b4efaeebd53d770d37363
pcmohammadhafeez <pcmohammadhafeez@gmail.com>
2026-08-16 19:19:55 +0530

    Add user defined alarms

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -262,7 +262,7 @@ function RootComponent() {
 
             <InstallPrompt />
 
-            <PrayerNotificationScheduler />
+            <PrayerNotificationScheduler />`r`n <UserAlarm />
 
             <OfflineNotice />
           </ContentProvider>
